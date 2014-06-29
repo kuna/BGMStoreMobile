7031e4a6619a4b68c5011ea5b21f941f8d0a9ec3
ChaYoung You <yousbe@gmail.com>
2014-06-30 07:19:43 +0900

    Make Github TextView look like a link

--- src/com/kuna/bgmstoremobile/Info.java
+++ src/com/kuna/bgmstoremobile/Info.java
@@ -1,11 +1,8 @@
 package com.kuna.bgmstoremobile;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class Info extends Activity {
@@ -15,11 +12,6 @@ public class Info extends Activity {
         setContentView(R.layout.infoview);
 
         TextView t = (TextView) findViewById(R.id.textView2);
-        t.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/kuna/BGMStoreMobile")));
-            }
-        });
+        t.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
