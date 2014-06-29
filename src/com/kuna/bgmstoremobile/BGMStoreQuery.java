fca63ca61787bc55fc5c76f6f8e81a2de6dcbbf4
ChaYoung You <yousbe@gmail.com>
2014-06-30 07:40:23 +0900

    Return the result without making assignments

--- src/com/kuna/bgmstoremobile/BGMStoreQuery.java
+++ src/com/kuna/bgmstoremobile/BGMStoreQuery.java
@@ -14,13 +14,13 @@ public class BGMStoreQuery {
     }
 
     public static String getQuery() {
-        String r = "limit_count=" + Integer.toString(sLimitCount)
+        return "limit_count=" + Integer.toString(sLimitCount)
                 + "&q=" + sKeyword
                 + "&q_type=" + sKeywordType
                 + "&sort_by=" + sSortBy
                 + "&sort_type=" + sSortType
                 + "&member_num=" + ((sMemberNum >= 0) ? Integer.toString(sMemberNum) : "")
                 + "&q_mode=" + sQuoryMode;
-        return r; //URLEncoder.encode(r);
+        //URLEncoder.encode(r);
     }
 }
